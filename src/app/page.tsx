526d3bd5e8b413fbb7c7ba26bc6ecc14b64e4a51
Steven Uy <81347262+Steven-UY@users.noreply.github.com>
2024-12-28 13:36:46 +0800

    made the background dotted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className = "min-h-screen flex flex-col">
+    <div className = "min-h-screen flex flex-col h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
       <Header></Header>
       <main className = "flex-grow">
       <Content></Content>
