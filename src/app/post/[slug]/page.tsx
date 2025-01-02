2aaa4378be0bd39e224e54e73d832cf7624431d4
Steven Uy <81347262+Steven-UY@users.noreply.github.com>
2025-01-02 22:32:37 +0800

    resolved async and await errors

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -4,49 +4,40 @@ import getPostMetadata from "../../../../utils/getPostMetadata"
 import fs from 'fs'
 import matter from 'gray-matter'
 
-type PostContent = {
-    content: string,
-    data: {
-        title: string,
-        date: string
-    }
-}
-
-type PostPageProps = {
+interface PostPage {
     params: {
         slug: string
-    }
-    searchParams?: Record<string, string>
+    };
+    searchParams: { [key: string]: string | string[] | undefined};
 }
 
-//fetch content from markdown file that matches the slug
-function getPostContent(slug: string): PostContent{
+function getPostContent(slug: string) {
     const folder = 'posts/'
     const file = folder + `${slug}.md`
     const content = fs.readFileSync(file, 'utf8')
 
-    const matterResult = matter(content) as unknown as PostContent;
-    return matterResult;
+    const matterResult = matter(content)
+    return matterResult
 }
 
-//fetch metadata of the page
-export const generateStaticParams = async (): Promise<Array<{ slug: string }>> => {
-    const posts = getPostMetadata('posts');
-    return posts.map((post) => ({ slug: post.slug }));
+export const generateStaticParams = async () => {
+    const posts = getPostMetadata('posts')
+    return posts.map((post) => ({ slug: post.slug}))
 }
 
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
-    const { slug } = params
-    const id = slug ? ' ⋅ ' + slug : '';
+export async function generateMetadata({ params, searchParams }: PostPage) {
+    const resolvedParams = await params
+    const id = resolvedParams?.slug ? ' * ' + resolvedParams?.slug: '' 
     return {
-        title: `The Garbage Dump ${id.replaceAll('_', ' ')}`
-    };
+        title: `The Garbage Dump ${id.replaceAll('-', ' ')}`,
+    }
 }
 
-export default async function PostPage({ params }: PostPageProps){
-    const { slug } = params
+export default async function PostPage(props: PostPage) {
+    const params = await props.params
+    const slug = params.slug
     const post = getPostContent(slug)
-
+    console.log(post)
     return (
         <main>
             <article>
