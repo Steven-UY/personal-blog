bca576a3f470ad3e4e252fe39cb9edb54a8ac87e
Steven Uy <81347262+Steven-UY@users.noreply.github.com>
2025-01-12 17:41:33 +0800

    footer is done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,31 +3,44 @@ import React from 'react'
 const Footer = () => { 
     return (
         <footer className="text-card-foreground shadow-md">
-            <h2 className="pt-5 text-xl">The Garbage Dump</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 py-12">
+            <h2 className="pt-3 text-lg">The Garbage Dump</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 py-6">
                 <div className="flex flex-col items-center sm:items-start">
-                    <ul>
-                        <li><a href="mailto:steven.uy10@gmail.com">steven.uy10@gmail.com</a></li>
+                    <ul className="space-y-2">
+                        <p>A blog</p>
+                        <li className="hover:underline">
+                            <a href="mailto:steven.uy10@gmail.com">steven.uy10@gmail.com</a>
+                        </li>
                     </ul>
                 </div>
                 <div className="flex flex-col items-center">
-                    <ul>
+                    <ul className="space-y-2">
                         <li>
-                            <a href="https://github.com/Steven-UY" className="flex items-center gap-2">
-                                <img 
-                                    src="/assets/Github_Light.png" 
-                                    alt="GitHub"
-                                    width={20}
-                                    height={20}
+                            <a href="https://github.com/Steven-UY" 
+                               className="flex items-center gap-2 hover:underline">
+                                <img src="/assets/Github_Light.png" 
+                                     alt="GitHub"
+                                     width={20}
+                                     height={20}
+                                />
+                                <span>slicko</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/steven-c-uy/" 
+                               className="flex items-center gap-2 hover:underline">
+                                <img src="/assets/linkedin.png" 
+                                     alt="LinkedIn"
+                                     width={20}
+                                     height={20}
                                 />
                                 <span>Steven Uy</span>
                             </a>
                         </li>
-                        <li><a></a></li>
                     </ul>
                 </div>
-                <div className="flex flex-col items-center sm:items-end">
-                    <p>A page for my terribly thought out ideas</p>
+                <div className="flex flex-col items-center sm:items-end text-sm">
+                    <p>A home for some ideas of mine. Maybe it'll be better than journaling.</p>
                 </div>
             </div>
         </footer>
